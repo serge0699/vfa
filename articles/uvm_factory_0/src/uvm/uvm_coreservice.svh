a0983988bc648054d9c40394eb2b7fc81a1d95d1
Maxim Karpukhin <50838885+karp013@users.noreply.github.com>
2025-03-18 13:58:42 +0300

    Fix, Style: Исправление опечаток, стилистические правки (#8)

--- articles/uvm_factory_0/src/uvm/uvm_coreservice.svh
+++ articles/uvm_factory_0/src/uvm/uvm_coreservice.svh
@@ -4,32 +4,32 @@
 // основным сервисам UVM. В том числе к фабрике.
 class uvm_coreservice_t;
 
-	// Защищенный указатель на единственный
-	// экземпляр типа ~uvm_coreservice_t~.
-	local static uvm_coreservice_t inst;
+    // Защищенный указатель на единственный
+    // экземпляр типа ~uvm_coreservice_t~.
+    local static uvm_coreservice_t inst;
 
-	// Функция получения указателя на единствен-
-	// ный экземпляр типа ~uvm_coreservice_t~.
-	static function uvm_coreservice_t get();
-		if(inst == null) begin
-			inst = new();
+    // Функция получения указателя на единствен-
+    // ный экземпляр типа ~uvm_coreservice_t~.
+    static function uvm_coreservice_t get();
+        if(inst == null) begin
+            inst = new();
         end
-		return inst;
-	endfunction
+            return inst;
+    endfunction
 
-	// Защищенный указатель на единственный
-	// экземпляр типа ~uvm_factory~. Дан-
-	// ный класс предоставляет доступ к фаб-
-	// рике.
+    // Защищенный указатель на единственный
+    // экземпляр типа ~uvm_factory~. Дан-
+    // ный класс предоставляет доступ к фаб-
+    // рике.
     local uvm_factory factory;
 
-	// Функция получения указателя на единствен-
-	// ный экземпляр типа ~uvm_factory~.
+    // Функция получения указателя на единствен-
+    // ный экземпляр типа ~uvm_factory~.
     virtual function uvm_factory get_factory();
-	    if(factory == null) begin
-	    	factory = new();
-	    end 
-		return factory;
-	endfunction
+        if(factory == null) begin
+            factory = new();
+        end 
+            return factory;
+        endfunction
 
 endclass
